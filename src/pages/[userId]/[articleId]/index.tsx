9df2847bc81ac55384c850d2b92a07abe7c744cb
Takuya Kurihara <takuya.kurihara00@gmail.com>
2020-09-27 20:21:58 +0900

    :sparkles: Add page of the article.

---
+++ src/pages/[userId]/[articleId]/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useGetArticleQuery } from '@/generated/graphql';
+
+const Index: NextPage = () => {
+  const router = useRouter();
+  const { articleId } = router.query;
+
+  const { loading, error, data} = useGetArticleQuery({
+    variables: {
+      id: articleId as string,
+    }
+  });
+
+  if (loading) {
+    return <p>...loading</p>
+  }
+
+  if (error) {
+    return <p>{error.toString()}</p>
+  }
+
+  return (
+    <div>
+      {JSON.stringify(data)}
+    </div>
+  );
+};
+
+export default Index;

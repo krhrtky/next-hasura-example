8036bf85447d6ad97b39b17212c9bf1ae1ff7325
Takuya Kurihara <takuya.kurihara00@gmail.com>
2020-10-03 18:43:59 +0900

    :sparkles: Add behavior resize textarea element auto.

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useRef } from 'react';
 import { NextPage } from 'next';
+import autosize from 'autosize';
 import styles from './index.module.css';
 
 const PostPage: NextPage = () => {
+  const ref = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (ref.current != null) {
+      autosize(ref.current);
+    }
+  }, []);
+
   return (
     <div className={styles.editContent}>
       <input className={styles.subject} type="text" placeholder="タイトル" />
-      <textarea className={styles.editor} placeholder="本文を書きましょう" />
+      <textarea
+        className={styles.editor}
+        placeholder="本文を書きましょう"
+        ref={ref}
+      />
     </div>
   )
 }

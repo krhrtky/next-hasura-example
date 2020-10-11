fb6acee1ad3087409489d1648fc8488bf929be82
Takuya Kurihara <takuya.kurihara00@gmail.com>
2020-10-11 20:09:00 +0900

    :hammer_and_pick: Delete unnecessary console.log .

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -14,13 +14,11 @@ export const Paragraph: React.FC<ParagraphProps> = ({ p }) => (
 type ArticleProps = {
   content: string;
 };
-export const Article: React.FC<ArticleProps> = ({ content }) => {
-  console.log(content.split(/\n/))
-  return (
-    <>
-      {formatArticle(content).map((p, i) => (
-        <Paragraph p={p} key={i} />
-      ))}
-    </>
-  );
-}
+export const Article: React.FC<ArticleProps> = ({ content }) => (
+  <>
+    {formatArticle(content).map((p, i) => (
+      <Paragraph p={p} key={i} />
+    ))}
+  </>
+);
+
